1a1fd6d96685e7eaa9d98220a77fd1eb6179831d
Milton Fragoso <fragosomilton54@gmail.com>
2022-07-11 08:51:14 -0300

    Tipos de Data Bindings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './navegacao/menu/menu.component'
 import { HomeComponent } from './navegacao/home/home.component';
 import { FooterComponent } from './navegacao/footer/footer.component';
 import { rootRouterConfig } from './app.routes';
+import { DataBindingComponent } from './demos/data-binding/data-binding.component';
 
 
 @NgModule({
@@ -15,11 +17,13 @@ import { rootRouterConfig } from './app.routes';
     AppComponent,
     MenuComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    DataBindingComponent
   ],
   imports: [
     BrowserModule,
-    [RouterModule.forRoot(rootRouterConfig, { useHash: false})]
+    [RouterModule.forRoot(rootRouterConfig, { useHash: false})],
+    FormsModule
   ],
   providers: [
     { provide: APP_BASE_HREF, useValue: '/'}
